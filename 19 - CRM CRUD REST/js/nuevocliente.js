a311fd48d0480ad65a1f42b8241165ca5bb4e10d
rcastaneda1491 <rcastaneda1491@gmail.com>
2021-07-19 10:09:13 -0600

    Subir proyecto 19

---
+++ 19 - CRM CRUD REST/js/nuevocliente.js
@@ -0,0 +1,41 @@
+import { nuevoCliente } from './API.js';
+import { mostrarAlerta } from './funciones.js';
+
+(function() {
+    const formulario = document.querySelector('#formulario');
+    formulario.addEventListener('submit', validarCliente);
+
+    async function validarCliente(e) {
+        e.preventDefault();
+
+        const nombre = document.querySelector('#nombre').value;
+        const nacionalidad = document.querySelector('#nacionalidad').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        const cliente = {
+            nombre, 
+            nacionalidad,
+            email, 
+            telefono,
+            empresa
+        }
+
+        if( validar(cliente) ) {
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+        await nuevoCliente(cliente);
+        window.location.href = 'index.html';
+    }
+
+
+   
+
+    function validar(obj) {
+        return !Object.values(obj).every(element => element !== '') ;
+    }
+
+   
+})();
